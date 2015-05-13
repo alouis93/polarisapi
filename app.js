27fb5158cb2f56e295846cd12b23306f576ec3b3
Andrew Louis <andrew.louis93@gmail.com>
2015-05-13 11:22:25 -0400

    13

--- app.js
+++ app.js
@@ -58,10 +58,7 @@ function constructTwiml(map, directions) {
   //api_cache.push(map);
   api_cache.push(directions);
   api_cache.push(JSON.stringify(directions));
-  twimlRes += '<Response><Message>' +
-    // '<Map>' + JSON.stringify(map) + '</Map>' +
-    + JSON.stringify(directions) +
-    '</Message></Response>';
+  twimlRes += '<Response><Message>' +  JSON.stringify(directions) + '</Message></Response>';
   return twimlRes;
 }
 
