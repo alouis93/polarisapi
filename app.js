422d2533dd73d56c5f264e3d773a58e6593bb618
Andrew Louis <andrew.louis93@gmail.com>
2015-05-17 19:01:00 -0400

    Grabbing numbers III

--- app.js
+++ app.js
@@ -39,8 +39,10 @@ app.post('/api/sms', function(req, res) {
       .replace(')', '}')
     );
 
-  // api_cache.push( JSON.stringify(req.body.To) );
-  api_cache.push( JSON.stringify(req.body.From) );
+
+
+  api_cache.push( JSON.parse(req.body.To) );
+  // api_cache.push( JSON.stringify(req.body.From) );
   // var data = req.body;
   // console.log(req.body);
 
